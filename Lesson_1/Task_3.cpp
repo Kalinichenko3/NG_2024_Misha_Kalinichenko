7ea75f9e3f676f09629dbf012d41a98a8ca85c0e
Kalinichenko3 <kalinichenko.m03@gmail.com>
2024-11-11 23:42:40 +0200

    Added Lesson_1/Task_3.cpp

---
+++ Lesson_1/Task_3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "-------------------" << endl
+        << "Vvedite pervoe chislo: " ;
+    cin >> a;
+    cout << "Vvedite vtoroe chislo: ";
+    cin >> b;
+        
+
+    cout << "-------------------" << endl
+        << "1 - summa" << endl 
+        << "2 - raznica" << endl
+        << "3 - umnogenie" << endl
+        << "4 - delenie" << endl 
+        <<  "-------------------" << endl
+        << "Vvedite deistwie: " << endl;
+    cin >> c;
+    switch(c)
+    {
+    case 1:
+        cout << a + b;
+        break;
+    case 2:
+        cout << a - b;
+        break;
+    case 3:
+        cout << a * b;
+        break;
+    case 4:
+        cout << a / b;
+        break;
+    default:
+        cout << "eror";
+    }
+    return 0;
+}
