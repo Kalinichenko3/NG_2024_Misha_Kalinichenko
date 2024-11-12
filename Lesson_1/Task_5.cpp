a5e35e436117ab66806172ea6b550f0f521ba0bc
Kalinichenko3 <kalinichenko.m03@gmail.com>
2024-11-13 01:22:34 +0200

    Added Lesson_1/Task_5.cpp

---
+++ Lesson_1/Task_5.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+using namespace std;
+int main()
+
+{
+    int userInput = 0;
+    cout << "enter number: "; 
+    cin >> userInput;
+    int buffer = userInput;
+    for (int vertical = 0; vertical != userInput; vertical++) {
+        for (int space = buffer - 1; space != 0; space--) {
+            cout << " ";
+        }
+        buffer--;
+        for (int horisontal = 0; horisontal != vertical * 2 + 1; horisontal++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int penyok = 0; penyok != userInput-1; penyok++) {
+        cout << " ";
+    }
+    cout << "*";
+    
+}
